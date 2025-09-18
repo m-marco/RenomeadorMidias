62fe86f1970b8bd56808cf87d92eeaf43dbd2cd5
Marco Jr <marco1002@live.com>
2025-09-18 19:53:39 -0300

    finalizando acesso aos metadados

--- RenomeadorMidias/Program.cs
+++ RenomeadorMidias/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 Console.WriteLine("Bem vindo ao renomeador de mídias! Digite a pasta onde estão as mídias para começarmos:");
 
 string path = Console.ReadLine();
-
-if (string.IsNullOrEmpty(path))
-    path = "C:\\Users\\Marco\\Videos\\JWLibrary";
 
 if (!Directory.Exists(path))
 {
@@ -16,30 +13,43 @@ if (!Directory.Exists(path))
     return;
 }
 
+string returnPath = Path.Combine(path, "Renomeados");
+
+if (!Directory.Exists(returnPath))
+    Directory.CreateDirectory(returnPath);
+
 var files = Directory.GetFiles(path);
 
 foreach (var item in files)
 {
     try
     {
-        using (var tagFile = TagLib.File.Create(item))
-        {
-            // A propriedade Tag contém metadados como Título, Artista, Álbum, etc.
-            string title = tagFile.Tag.Title;
+        using var tagFile = TagLib.File.Create(item);
+        // A propriedade Tag contém metadados como Título, Artista, Álbum, etc.
+        string title = tagFile?.Tag?.Title;
 
-            if (!string.IsNullOrEmpty(title))
-            {
-                Console.WriteLine($"Arquivo: {Path.GetFileName(item)} | Título: {title}");
-            }
-            else
-            {
-                Console.WriteLine($"Arquivo: {Path.GetFileName(item)} não possui um título nos metadados.");
-            }
+        if (!string.IsNullOrEmpty(title))
+        {
+            Console.WriteLine($"Arquivo: {Path.GetFileName(item)} | Título: {title}");
+            File.Copy(item, Path.Combine(returnPath, SanitizeFileName(title) + Path.GetExtension(item)), true);
+        }
+        else
+        {
+            Console.WriteLine($"Arquivo: {Path.GetFileName(item)} não possui um título nos metadados.");
         }
     }
     catch (Exception ex)
     {
-        Console.WriteLine($"Erroao ler dados do arquivo {item} {ex.Message}");
+        Console.WriteLine($"Erro ao ler dados do arquivo {item} {ex.Message}");
     }
 }
+string SanitizeFileName(string fileName)
+{
+    char[] invalidChars = Path.GetInvalidFileNameChars();
 
+    // Remove os caracteres inválidos
+    string sanitized = new(fileName.Where(ch => !invalidChars.Contains(ch)).ToArray());
+
+    // Remove espaços ou pontos no final do nome
+    return sanitized.TrimEnd(' ', '.');
+}
